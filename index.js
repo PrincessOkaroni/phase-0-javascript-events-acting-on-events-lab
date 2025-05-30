63976ccd9edaaa864727823eefd8212caa63a623
Purity Nekesa <nekesapurity20@gmail.com>
2025-05-30 17:22:46 +0300

    modified js dodger

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left < 360) {
+    // assuming the game field is 400px wide and dodger is 40px wide
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+if (typeof module !== "undefined") {
+  module.exports = { moveDodgerLeft, moveDodgerRight };
+}
